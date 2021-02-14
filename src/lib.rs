8e8a079641a6217a54f8f40e7d5b161e053d4576
Weird Constructor <weirdconstructor@gmail.com>
2021-02-14 11:03:26 +0100

    setup boilerplate with baseplug

---
+++ src/lib.rs
@@ -0,0 +1,104 @@
+#![allow(incomplete_features)]
+#![feature(generic_associated_types)]
+
+use serde::{Serialize, Deserialize};
+
+use baseplug::{
+    ProcessContext,
+    Plugin,
+};
+
+
+baseplug::model! {
+    #[derive(Debug, Serialize, Deserialize)]
+    struct HexoSynthModel {
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "A1")]
+        mod_a1: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "A2")]
+        mod_a2: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "A3")]
+        mod_a3: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "A4")]
+        mod_a4: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "A5")]
+        mod_a5: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "A6")]
+        mod_a6: f32,
+
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "B1")]
+        mod_b1: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "B2")]
+        mod_b2: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "B3")]
+        mod_b3: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "B4")]
+        mod_b4: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "B5")]
+        mod_b5: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "B6")]
+        mod_b6: f32,
+    }
+}
+
+impl Default for HexoSynthModel {
+    fn default() -> Self {
+        Self {
+            mod_a1: 0.0,
+            mod_a2: 0.0,
+            mod_a3: 0.0,
+            mod_a4: 0.0,
+            mod_a5: 0.0,
+            mod_a6: 0.0,
+
+            mod_b1: 0.0,
+            mod_b2: 0.0,
+            mod_b3: 0.0,
+            mod_b4: 0.0,
+            mod_b5: 0.0,
+            mod_b6: 0.0,
+        }
+    }
+}
+
+struct HexoSynth;
+
+impl Plugin for HexoSynth {
+    const NAME:    &'static str = "Hexagonal Modular Synthesizer";
+    const PRODUCT: &'static str = "Hexagonal Modular Synthesizer";
+    const VENDOR:  &'static str = "Weird Constructor";
+
+    const INPUT_CHANNELS: usize = 2;
+    const OUTPUT_CHANNELS: usize = 2;
+
+    type Model = HexoSynthModel;
+
+    #[inline]
+    fn new(_sample_rate: f32, _model: &HexoSynthModel) -> Self {
+        Self
+    }
+
+    #[inline]
+    fn process(&mut self, model: &HexoSynthModelProcess, ctx: &mut ProcessContext<Self>) {
+        let input = &ctx.inputs[0].buffers;
+        let output = &mut ctx.outputs[0].buffers;
+
+        for i in 0..ctx.nframes {
+            output[0][i] = input[0][i] * model.mod_a1[i];
+            output[1][i] = input[1][i] * model.mod_a1[i];
+        }
+    }
+}
+
+baseplug::vst2!(HexoSynth, b"HxsY");
