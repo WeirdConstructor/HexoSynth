6b12ec85692c2dd42856a37438b12ce5fb24274e
Weird Constructor <weirdconstructor@gmail.com>
2021-03-09 18:57:55 +0100

    standalone example works now properly (with jack)

--- examples/standalone.rs
+++ examples/standalone.rs
@@ -2,58 +2,175 @@ use hexotk::*;
 use hexotk::widgets::*;
 use hexosynth::ui::matrix::NodeMatrixData;
 use hexosynth::*;
+use hexosynth::nodes::NodeExecutor;
 
 use std::rc::Rc;
 use std::sync::Arc;
+use std::sync::Mutex;
 
+struct Notifications {
+    node_exec: Arc<Mutex<NodeExecutor>>,
+}
 
-use cpal::{Data, Sample, SampleFormat};
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-
-
-fn start_backend(shared: Arc<HexoSynthShared>) {
-    let host = cpal::default_host();
-    let device = host.default_output_device().expect("no output device available");
-
-    let mut supported_configs_range =
-        device.supported_output_configs()
-            .expect("error while querying configs");
-
-    let mut config = None;
-    while let Some(f) = supported_configs_range.next() {
-        if f.max_sample_rate().0 > 44100
-           && f.sample_format() == SampleFormat::F32
-           && f.channels() == 2
-        {
-            println!("Config found: {:?}", f);
-            config = Some(f.with_sample_rate(cpal::SampleRate(44100)));
-        }
+impl jack::NotificationHandler for Notifications {
+    fn thread_init(&self, _: &jack::Client) {
+        println!("JACK: thread init");
     }
 
-    let config = config.expect("Finding some F32, 44100Hz 2 Channel config!");
+    fn shutdown(&mut self, status: jack::ClientStatus, reason: &str) {
+        println!(
+            "JACK: shutdown with status {:?} because \"{}\"",
+            status, reason
+        );
+    }
+
+    fn freewheel(&mut self, _: &jack::Client, is_enabled: bool) {
+        println!(
+            "JACK: freewheel mode is {}",
+            if is_enabled { "on" } else { "off" }
+        );
+    }
+
+    fn buffer_size(&mut self, _: &jack::Client, sz: jack::Frames) -> jack::Control {
+        println!("JACK: buffer size changed to {}", sz);
+        jack::Control::Continue
+    }
+
+    fn sample_rate(&mut self, _: &jack::Client, srate: jack::Frames) -> jack::Control {
+        println!("JACK: sample rate changed to {}", srate);
+        let mut ne = self.node_exec.lock().unwrap();
+        ne.set_sample_rate(srate as f32);
+        jack::Control::Continue
+    }
+
+    fn client_registration(&mut self, _: &jack::Client, name: &str, is_reg: bool) {
+        println!(
+            "JACK: {} client with name \"{}\"",
+            if is_reg { "registered" } else { "unregistered" },
+            name
+        );
+    }
+
+    fn port_registration(&mut self, client: &jack::Client, port_id: jack::PortId, is_reg: bool) {
+        if let Some(p) = client.port_by_id(port_id) {
+            if let Ok(name) = p.name() {
+                println!("JACK: port registered: {}", name);
+//                if name == "HexoSynth:hexosynth_out1" {
+//                    client.connect_ports_by_name(&name, "system:playback_1");
+//                } else if name == "HexoSynth:hexosynth_out2" {
+//                    client.connect_ports_by_name(&name, "system:playback_2");
+//                }
+            }
+        }
+        println!(
+            "JACK: {} port with id {}",
+            if is_reg { "registered" } else { "unregistered" },
+            port_id
+        );
+    }
+
+    fn port_rename(
+        &mut self,
+        _: &jack::Client,
+        port_id: jack::PortId,
+        old_name: &str,
+        new_name: &str,
+    ) -> jack::Control {
+        println!(
+            "JACK: port with id {} renamed from {} to {}",
+            port_id, old_name, new_name
+        );
+        jack::Control::Continue
+    }
+
+    fn ports_connected(
+        &mut self,
+        _: &jack::Client,
+        port_id_a: jack::PortId,
+        port_id_b: jack::PortId,
+        are_connected: bool,
+    ) {
+        println!(
+            "JACK: ports with id {} and {} are {}",
+            port_id_a,
+            port_id_b,
+            if are_connected {
+                "connected"
+            } else {
+                "disconnected"
+            }
+        );
+    }
+
+    fn graph_reorder(&mut self, _: &jack::Client) -> jack::Control {
+        println!("JACK: graph reordered");
+        jack::Control::Continue
+    }
+
+    fn xrun(&mut self, _: &jack::Client) -> jack::Control {
+        println!("JACK: xrun occurred");
+        jack::Control::Continue
+    }
+
+    fn latency(&mut self, _: &jack::Client, mode: jack::LatencyType) {
+        println!(
+            "JACK: {} latency has changed",
+            match mode {
+                jack::LatencyType::Capture => "capture",
+                jack::LatencyType::Playback => "playback",
+            }
+        );
+    }
+}
+
+
+fn start_backend<F: FnMut()>(shared: Arc<HexoSynthShared>, mut f: F) {
+    let (client, _status) =
+        jack::Client::new("HexoSynth", jack::ClientOptions::NO_START_SERVER)
+        .unwrap();
+
+    let in_a =
+        client.register_port("hexosynth_in1", jack::AudioIn::default())
+            .unwrap();
+    let in_b =
+        client.register_port("hexosynth_in2", jack::AudioIn::default())
+            .unwrap();
+    let mut out_a =
+        client.register_port("hexosynth_out1", jack::AudioOut::default())
+            .unwrap();
+    let mut out_b =
+        client.register_port("hexosynth_out2", jack::AudioOut::default())
+            .unwrap();
 
     let mut node_exec = shared.node_exec.borrow_mut().take().unwrap();
-    node_exec.set_sample_rate(config.sample_rate().0 as f32);
+    let ne = Arc::new(Mutex::new(node_exec));
+    let ne2 = ne.clone();
 
-    let mut l = vec![0.0_f32; 1024];
-    let mut r = vec![0.0_f32; 1024];
-    let li = vec![0.0_f32; 1024];
-    let ri = vec![0.0_f32; 1024];
+    let mut first = true;
+    let process_callback = move |client: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
+        let out_a_p = out_a.as_mut_slice(ps);
+        let out_b_p = out_b.as_mut_slice(ps);
+        let in_a_p = in_a.as_slice(ps);
+        let in_b_p = in_b.as_slice(ps);
 
-    let mut fun = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-        println!("XX");
-        let mut obuf = [&mut l[..], &mut r[..]];
-        let mut ibuf = [&li[..], &ri[..]];
+        if first {
+            client.connect_ports_by_name("HexoSynth:hexosynth_out1", "system:playback_1");
+            client.connect_ports_by_name("HexoSynth:hexosynth_out2", "system:playback_2");
+            first = false;
+        }
 
-        node_exec.process_graph_updates();
+
+        let nframes = out_a_p.len();
 
         let mut context = Context {
             frame_idx: 0,
-            output: &mut obuf[..],
-            input: &ibuf[..],
+            output: &mut [out_a_p, out_b_p],
+            input: &[in_a_p, in_b_p],
         };
 
-        let nframes = data.len() / 2;
+        let mut node_exec = ne.lock().unwrap();
+
+        node_exec.process_graph_updates();
 
         for i in 0..nframes {
             context.frame_idx    = i;
@@ -61,41 +178,41 @@ fn start_backend(shared: Arc<HexoSynthShared>) {
             context.output[1][i] = 0.0;
 
             node_exec.process(&mut context);
-
-            data[0] = context.output[0][i];
-            data[1] = context.output[1][i];
         }
+
+        jack::Control::Continue
     };
 
-    let config : cpal::StreamConfig = config.into();
-    let stream = device.build_output_stream(
-        &config,
-        fun,
-        move |err| {
-            eprintln!("an error occurred on the output audio stream: {}", err);
-            // react to errors here.
-        },
-    ).expect("Stream to be built nicely");
-    stream.play().expect("The stream to play...");
-    println!("Started audio!");
+    let process =
+        jack::ClosureProcessHandler::new(process_callback);
+
+    // Activate the client, which starts the processing.
+    let active_client =
+        client.activate_async(Notifications {
+            node_exec: ne2,
+        }, process)
+            .unwrap();
+
+    f();
+
+    active_client.deactivate().unwrap();
 }
 
 fn main() {
     let shared = Arc::new(HexoSynthShared::new());
 
-    start_backend(shared.clone());
+    start_backend(shared.clone(), move || {
+        let matrix = shared.matrix.clone();
 
-    let matrix = shared.matrix.clone();
+        open_window("HexoTK Standalone", 1400, 700, None, Box::new(|| {
 
-    open_window("HexoTK Standalone", 1400, 700, None, Box::new(|| {
+            let mut ui = Box::new(UI::new(
+                Box::new(NodeMatrixData::new(matrix, UIPos::center(12, 12), 11)),
+                Box::new(HexoSynthUIParams::new()),
+                (1400 as f64, 700 as f64),
+            ));
 
-        let mut ui = Box::new(UI::new(
-            Box::new(NodeMatrixData::new(matrix, UIPos::center(12, 12), 11)),
-            Box::new(HexoSynthUIParams::new()),
-            (1400 as f64, 700 as f64),
-        ));
-
-        ui
-    }));
-
+            ui
+        }));
+    });
 }
