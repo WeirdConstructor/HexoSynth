9c18f66fc7561565b0b0f3cd8afaba275a652596
Weird Constructor <weirdconstructor@gmail.com>
2021-04-23 05:09:43 +0200

    finished probability test

--- src/dsp/tracker/sequencer.rs
+++ src/dsp/tracker/sequencer.rs
@@ -11,6 +11,7 @@ pub struct PatternSequencer {
     col_types:  [PatternColType; 6],
     data:       Vec<Vec<f32>>,
     rng:        SplitMix64,
+    rand_vals:  [(usize, f64); 6],
 }
 
 const FRACT_16THS : [f32; 16] = [
@@ -38,7 +39,8 @@ impl PatternSequencer {
             rows,
             col_types: [PatternColType::Value; 6],
             data:      vec![vec![0.0; MAX_PATTERN_LEN]; 6],
-            rng:       SplitMix64::new(0x123456789),
+            rng:       SplitMix64::new(0x91234),
+            rand_vals: [(99999, 0.0); 6],
         }
     }
 
@@ -56,6 +58,7 @@ impl PatternSequencer {
             col_types: [PatternColType::Value; 6],
             data:      vec![vec![0.0; MAX_PATTERN_LEN]; 6],
             rng:       SplitMix64::new_from_i64(seed),
+            rand_vals: [(99999, 0.0); 6],
         }
     }
 
@@ -107,11 +110,11 @@ impl PatternSequencer {
     }
 
     pub fn col_gate_at_phase(
-        &mut self, col: usize, phase: &[f32], out: &mut [f32])
+        &mut self, col_idx: usize, phase: &[f32], out: &mut [f32])
     {
-        let col = &self.data[col][..];
+        let col = &self.data[col_idx][..];
 
-        let last_row_idx : f32 = (self.rows as f32) - 0.000001;
+        let last_row_idx : f32 = (self.rows as f32) - 0.00001;
 
         for (phase, out) in phase.iter().zip(out.iter_mut()) {
             let row_phase  = phase.clamp(0.0, 1.0) * last_row_idx;
@@ -134,9 +137,9 @@ impl PatternSequencer {
             //      0x0  - Row is divided up into 16 Gates
             let row_div     : f32 = (((gate & 0x0F0) >> 4) + 1) as f32;
             // probability:
-            //      0xF  - Gate is always triggered
-            //      0x7  - Gate fires only in 50% of the cases
-            //      0x0  - Gate fires only in ~6% of the cases
+            //      0xF  - Row is always triggered
+            //      0x7  - Row fires only in 50% of the cases
+            //      0x0  - Row fires only in ~6% of the cases
             let probability : u8 = ((gate & 0xF00) >> 8) as u8;
 
             let sub_frac = (phase_frac * row_div).fract();
@@ -148,9 +151,17 @@ impl PatternSequencer {
             // TODO: FIXME: We need to calculate the random value
             //              once per row!
             if probability < 0xF {
-                if self.rng.next_open01()
-                   > (FRACT_16THS[probability as usize] as f64)
-                {
+                let rand_val =
+                    if self.rand_vals[col_idx].0 != line {
+                        let new_rand_val = self.rng.next_open01();
+                        self.rand_vals[col_idx] = (line, new_rand_val);
+                        new_rand_val
+                    } else {
+                        self.rand_vals[col_idx].1
+                    };
+                //d// println!("RANDVAL: {:?} | {:9.7}", self.rand_vals[col_idx], FRACT_16THS[probability as usize]);
+
+                if rand_val > (FRACT_16THS[probability as usize] as f64) {
                     continue;
                 }
             }
@@ -245,7 +256,7 @@ mod tests {
         ]);
 
         let mut out = [0.0; 6];
-        ps.col_gate_at_phase(0, &[0.1, 0.5, 0.500001, 0.6, 0.9, 0.99], &mut out[..]);
+        ps.col_gate_at_phase(0, &[0.1, 0.5, 0.5001, 0.6, 0.9, 0.99], &mut out[..]);
         //d// println!("out: {:?}", out);
 
         assert_float_eq!(out[0], 1.0);
@@ -404,17 +415,19 @@ mod tests {
         assert_eq!(count_up(  &out[256..384]),   1);
     }
 
-    #[test]
-    fn check_seq_gate_div_rng_1() {
-        let mut ps = PatternSequencer::new_default_seed(3);
-        ps.set_col(0, &[
-            f32::from_bits(0x00F0),
-            f32::from_bits(0x00F7),
-            f32::from_bits(0x00FF),
-        ]);
+    fn run_probability_test_for(prob: u32) -> (usize, usize) {
+        let rows = 100;
 
-        let mut phase = vec![0.0; 6 * 64];
-        let inc = 1.0 / ((6.0 * 64.0) - 1.0);
+        let mut ps = PatternSequencer::new_default_seed(rows);
+        let mut coldata = vec![0.0; rows];
+        for i in 0..coldata.len() {
+            coldata[i] = f32::from_bits(0x00FF | prob);
+        }
+        ps.set_col(0, &coldata[..]);
+
+        let mut samples = rows;
+        let mut phase = vec![0.0; samples];
+        let inc = 1.0 / ((samples as f32) - 1.0);
         let mut phase_run = 0.0;
         for p in phase.iter_mut() {
             *p = phase_run;
@@ -423,16 +436,31 @@ mod tests {
 
         //d// println!("PHASE: {:?}", phase);
 
-        let mut out = [0.0; 6 * 64];
+        let mut out = vec![0.0; samples];
         ps.col_gate_at_phase(0, &phase[..], &mut out[..]);
 
-        assert_eq!(count_high(&out[0..128]),  8);
-        assert_eq!(count_up(  &out[0..128]),  2);
+        (count_high(&out[..]), count_up(&out[..]))
+    }
 
-        assert_eq!(count_high(&out[128..256]), 64);
-        assert_eq!(count_up(  &out[128..256]),  2);
-
-        assert_eq!(count_high(&out[256..384]), 128);
-        assert_eq!(count_up(  &out[256..384]),   1);
+    #[test]
+    fn check_seq_gate_div_rng() {
+        // XXX: The result numbers are highly dependent on the
+        //      sampling rate inside run_probability_test_for().
+        assert_eq!(run_probability_test_for(0x000), (5,   5));
+        assert_eq!(run_probability_test_for(0x100), (12, 11));
+        assert_eq!(run_probability_test_for(0x200), (20, 18));
+        assert_eq!(run_probability_test_for(0x300), (26, 23));
+        assert_eq!(run_probability_test_for(0x400), (32, 26));
+        assert_eq!(run_probability_test_for(0x500), (38, 29));
+        assert_eq!(run_probability_test_for(0x600), (47, 29));
+        assert_eq!(run_probability_test_for(0x700), (56, 26));
+        assert_eq!(run_probability_test_for(0x800), (60, 25));
+        assert_eq!(run_probability_test_for(0x900), (66, 24));
+        assert_eq!(run_probability_test_for(0xA00), (70, 22));
+        assert_eq!(run_probability_test_for(0xB00), (79, 18));
+        assert_eq!(run_probability_test_for(0xC00), (84, 13));
+        assert_eq!(run_probability_test_for(0xD00), (93,  7));
+        assert_eq!(run_probability_test_for(0xE00), (96,  5));
+        assert_eq!(run_probability_test_for(0xF00), (100, 1));
     }
 }
