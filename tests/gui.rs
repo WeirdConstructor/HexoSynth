a6656815cfde73205bc3fc3c0ed73244405dac05
Weird Constructor <weirdconstructor@gmail.com>
2021-07-21 06:41:19 +0200

    extended the WLambda API for the GUI Driver

--- tests/gui.rs
+++ tests/gui.rs
@@ -3,6 +3,8 @@ use hexosynth::matrix::*;
 use hexosynth::nodes::new_node_engine;
 use hexosynth::dsp::*;
 
+use hexotk::constants::{dbgid2str, str2dbgid, dbgid_unpack};
+
 use std::sync::{Arc, Mutex};
 use std::rc::Rc;
 use std::cell::RefCell;
@@ -78,6 +80,53 @@ struct Ctx {
     matrix: Arc<Mutex<Matrix>>,
 }
 
+fn zone_type2vval(zt: ZoneType) -> wlambda::VVal {
+    match zt {
+        ZoneType::ValueDragFine       => VVal::vec1(VVal::new_str("value_drag_fine")),
+        ZoneType::ValueDragCoarse     => VVal::vec1(VVal::new_str("value_drag_coarse")),
+        ZoneType::TextInput           => VVal::vec1(VVal::new_str("text_input")),
+        ZoneType::Keyboard            => VVal::vec1(VVal::new_str("keyboard")),
+        ZoneType::AtomClick { .. }    => VVal::vec1(VVal::new_str("atom_click")),
+        ZoneType::HexFieldClick { pos, ..  } => {
+            VVal::vec2(
+                VVal::new_str("hex_field_click"),
+                VVal::ivec2(pos.0 as i64, pos.1 as i64))
+        },
+        ZoneType::Click { index } => {
+            VVal::vec2(VVal::new_str("click"), VVal::Int(index as i64))
+        },
+        ZoneType::Drag { index } => {
+            VVal::vec2(VVal::new_str("click"), VVal::Int(index as i64))
+        },
+    }
+}
+
+fn active_zone2vval(z: &ActiveZone) -> wlambda::VVal {
+    VVal::map3(
+        "id",   id_idx2vval(z.id, z.dbgid),
+        "pos",  VVal::fvec4(z.pos.x, z.pos.y, z.pos.w, z.pos.h),
+        "zone", zone_type2vval(z.zone_type))
+}
+
+fn id_idx2vval(id: AtomId, idx: usize) -> wlambda::VVal {
+    if idx > 0xFFFF {
+        let (idx, x, y) = dbgid_unpack(idx);
+
+        VVal::vec3(
+            VVal::pair(
+                VVal::Int(id.node_id() as i64),
+                VVal::Int(id.atom_id() as i64)),
+            VVal::new_str(dbgid2str(idx)),
+            VVal::ivec2(x as i64, y as i64))
+    } else {
+        VVal::vec2(
+            VVal::pair(
+                VVal::Int(id.node_id() as i64),
+                VVal::Int(id.atom_id() as i64)),
+            VVal::new_str(dbgid2str(idx)))
+    }
+}
+
 fn setup_environment() -> wlambda::GlobalEnvRef {
     use wlambda::{Env, VVal};
     let global_env = wlambda::GlobalEnv::new_default();
@@ -86,7 +135,103 @@ fn setup_environment() -> wlambda::GlobalEnvRef {
         "query_state", |env: &mut Env, argc: usize| {
         env.with_user_do(|ctx: &mut Ctx| {
             ctx.drv.query_state();
+//            println!("ZONES: {:#?}", ctx.drv.zones);
+//            println!("TEXTS: {:?}", ctx.drv.texts);
             Ok(VVal::None)
+        })
+    }, Some(0), Some(0));
+
+    global_env.borrow_mut().add_func(
+        "hover", |env: &mut Env, argc: usize| {
+        env.with_user_do(|ctx: &mut Ctx| {
+            if let Some(hz) = ctx.drv.hover {
+                Ok(active_zone2vval(&hz))
+            } else {
+                Ok(VVal::None)
+            }
+        })
+    }, Some(0), Some(0));
+
+    global_env.borrow_mut().add_func(
+        "zones", |env: &mut Env, argc: usize| {
+        env.with_user_do(|ctx: &mut Ctx| {
+            let ret = VVal::vec();
+            for z in ctx.drv.zones.iter() {
+                ret.push(active_zone2vval(z));
+            }
+
+            Ok(ret)
+        })
+    }, Some(0), Some(0));
+
+
+    global_env.borrow_mut().add_func(
+        "id_by_text_contains", |env: &mut Env, argc: usize| {
+        let needle = env.arg(0).s_raw();
+
+        env.with_user_do(|ctx: &mut Ctx| {
+            let ret = VVal::vec();
+
+            for ((id, idx), (s, pos)) in ctx.drv.texts.iter() {
+                if let Some(_) = s.find(&needle) {
+                    ret.push(
+                        VVal::vec3(
+                            id_idx2vval(*id, *idx),
+                            VVal::fvec4(pos.x, pos.y, pos.w, pos.h),
+                            VVal::new_str(s)));
+                }
+            }
+
+            Ok(if ret.len() == 0 { VVal::None } else { ret })
+        })
+    }, Some(1), Some(1));
+
+    global_env.borrow_mut().add_func(
+        "mouse_move", |env: &mut Env, argc: usize| {
+        let (x, y) = (
+            env.arg(0).v_f(0),
+            env.arg(0).v_f(1)
+        );
+        env.with_user_do(|ctx: &mut Ctx| {
+            ctx.drv.move_mouse(x, y);
+            Ok(VVal::None)
+        })
+    }, Some(1), Some(1));
+
+    global_env.borrow_mut().add_func(
+        "param_id", |env: &mut Env, argc: usize| {
+        let node_id = env.arg(0).with_s_ref(|s| NodeId::from_str(s));
+        let node_id = node_id.to_instance(env.arg(1).i() as usize);
+
+        let uniq_idx =
+            env.with_user_do(|ctx: &mut Ctx| {
+                let m = ctx.matrix.lock().unwrap();
+                m.unique_index_for(&node_id)
+            });
+
+        if let Some(uniq_idx) = uniq_idx {
+            if let Some(param_id) =
+                env.arg(2).with_s_ref(|s| node_id.inp_param(s))
+            {
+                Ok(VVal::pair(
+                    VVal::Int(uniq_idx as i64),
+                    VVal::Int(param_id.inp() as i64)))
+            }
+            else
+            {
+                Ok(VVal::None)
+            }
+        } else {
+            Ok(VVal::None)
+        }
+    }, Some(3), Some(3));
+
+    global_env.borrow_mut().add_func(
+        "mouse_pos", |env: &mut Env, argc: usize| {
+        env.with_user_do(|ctx: &mut Ctx| {
+            Ok(VVal::pair(
+                VVal::Flt(ctx.drv.mouse_pos.0),
+                VVal::Flt(ctx.drv.mouse_pos.1)))
         })
     }, Some(0), Some(0));
 
@@ -98,6 +243,9 @@ fn start_driver(matrix: Arc<Mutex<Matrix>>) -> Driver {
 
     std::thread::spawn(move || {
         use hexotk::constants::*;
+
+        std::thread::sleep(
+            std::time::Duration::from_millis(1000));
 
         let drvctx = Rc::new(RefCell::new(Ctx {
             drv: drv_frontend,
