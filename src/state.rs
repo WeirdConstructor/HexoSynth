b3e133376a3391fafef820cc93ff9d665ea04629
Weird Constructor <weirdconstructor@gmail.com>
2021-07-27 05:27:30 +0200

    started work on the state/event handling overhaul

---
+++ src/state.rs
@@ -0,0 +1,7 @@
+// Copyright (c) 2021 Weird Constructor <weirdconstructor@gmail.com>
+// This is a part of HexoSynth. Released under GPL-3.0-or-later.
+// See README.md and COPYING for details.
+
+pub struct State {
+    
+}
