59887e504ebc7aa386e2f93a36ee4f6212c4b406
Weird Constructor <weirdconstructor@gmail.com>
2021-07-27 21:04:14 +0200

    noted more map interaction ideas down

--- src/actions.rs
+++ src/actions.rs
@@ -55,27 +55,51 @@ impl Actions<'_, '_, '_> {
     pub fn map_messages_to_actions(&mut self, msg: &Msg) {
         match msg {
             Msg::CellDragged { btn, pos_a, pos_b } => {
-                // Left & pos_a exists & pos_b exists and are adjacent
-                //  => open menu, let the user select two in/outputs
-                //     and assign them
-                // Left & pos_a exists & pos_b exists and are NOT adjacent
-                //  => swap cells
                 // Left & pos_a exists & pos_b empty
-                //  => move cell
+                //  => move/swap cell
+                // Left & pos_a empty & pos_b exists
+                //  => open cell selection dialog for one node
+                //  => connect the default input
+                // Left & pos_a empty & pos_b empty & adjacent
+                //  => open cell selection dialog for two NodeIds
+                //  => connect the default output to default input
+                //     default is always: first input, first output
+                // Left & pos_a exists & pos_b exists & adjacent
+                //  => open connection selection dialog for out => inp
+                // Left & pos_a exists & pos_b exists & NOT adjacent
+                //  => take pos_a as output, pos_b as input
+                //  => search an empty input for pos_b
+                //  => copy cell at pos_a to that empty cell
+                //  => open connection dialog for out => inp
+
                 // Right & pos_a exists & pos_b empty & NOT adjacent
                 //  => copy cell, but with empty ports
                 // Right & pos_a exists & pos_b empty & adjacent
                 //  => copy cell, but with empty ports, open port connection dialog
-                // Right & pos_a exists & pos_b exists
-                //  => swap cells
-                // Middle & pos_a exists & pos_b empty & NOT adjacent
-                //  => new instance with empty ports
-                // Middle & pos_a exists & pos_b empty & adjacent
-                //  => new instance, but with empty ports, open port connection dialog
+                // Right & pos_a exists & pos_b exists & adjacent
+                //  => open connection menu for both
+                // Right & pos_a empty & pos_b empty & adjacent
+                //  => ????
+                // Right & pos_a empty & pos_b exists
+                //  => Delete pos_b
+                // Right & pos_a exists & pos_b exists & NOT adjacent
+                //  => take pos_a as output, pos_b as input
+                //  => search an empty input for pos_b
+                //  => NEW INSTANCE NodeId at pos_a to that empty cell
+                //  => open connection dialog for out => inp
+
                 let (src_cell, dst_cell) = (
                     self.matrix.get_copy(pos_a.0, pos_a.1),
                     self.matrix.get_copy(pos_b.0, pos_b.1)
                 );
+
+                // get_copy returns None for cells outside the matrix
+                let src_cell =
+                    if let Some(src_cell) = src_cell { src_cell }
+                    else { return; };
+                let dst_cell =
+                    if let Some(dst_cell) = dst_cell { dst_cell }
+                    else { return; };
 
                 let adjacent = CellDir::are_adjacent(*pos_a, *pos_b);
 
@@ -85,7 +109,24 @@ impl Actions<'_, '_, '_> {
                     if let Some(dir) = adjacent { dir.is_output() }
                     else { false };
 
-                match (*btn, src_cell, dst_cell, adjacent, src_is_output) {
+                let src =
+                    if src_cell.node_id() == NodeId::Nop { None }
+                    else { Some(src_cell) };
+                let dst =
+                    if dst_cell.node_id() == NodeId::Nop { None }
+                    else { Some(dst_cell) };
+
+                match (*btn, src, dst, adjacent, src_is_output) {
+                      (MButton::Left, Some(_), None, _, _)
+                    | (MButton::Left, None, Some(_), _, _) => {
+                        catch_err_dialog(self.dialog.clone(), || {
+                            self.matrix.change_matrix(|m| {
+                                m.place(pos_b.0, pos_b.1, src_cell);
+                                m.place(pos_a.0, pos_a.1, dst_cell);
+                            })?;
+                            m.sync()?;
+                        });
+                    },
                     (MButton::Left, Some(src), Some(dst), Some(dir), io) => {
                         println!("OPEN MENU!!!!! {:?} aisout={}",
                             dir, io);
