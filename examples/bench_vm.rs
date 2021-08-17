a6bf07426df0886ce33f44845cb5b8e13c63d880
Weird Constructor <weirdconstructor@gmail.com>
2021-08-17 06:17:32 +0200

    more experiments

--- examples/bench_vm.rs
+++ examples/bench_vm.rs
@@ -127,6 +127,42 @@ pub enum RegOp {
     Mul2(u8, u8, u8),
     Div(f32, u8, u8),
     Set(f32, u8),
+    Fo1,
+    Fo2,
+    Fo3,
+    Fo4,
+    Fo5,
+    Fo6,
+    Fo7,
+    Fo8,
+    Fo9,
+    Fo11,
+    Fo12,
+    Fo13,
+    Fo14,
+    Fo15,
+    Fo16,
+    Fo17,
+    Fo18,
+    Fo19,
+    Fo21,
+    Fo22,
+    Fo23,
+    Fo24,
+    Fo25,
+    Fo26,
+    Fo27,
+    Fo28,
+    Fo29,
+    Fo31,
+    Fo32,
+    Fo33,
+    Fo34,
+    Fo35,
+    Fo36,
+    Fo37,
+    Fo38,
+    Fo39,
 }
 
 pub struct RegVM {
@@ -138,6 +174,43 @@ impl RegVM {
         Self {
             regs: [0.0; 100],
         }
+    }
+
+    #[inline(never)]
+    pub fn add(&mut self, x: f32, a: usize, r: usize) {
+        self.regs[r] = self.regs[a] + x;
+    }
+
+    #[inline(never)]
+    pub fn mul(&mut self, x: f32, a: usize, r: usize) {
+        self.regs[r] = self.regs[a] * x;
+    }
+
+    #[inline(never)]
+    pub fn mul2(&mut self, b: usize, a: usize, r: usize) {
+        self.regs[r] = self.regs[a] * self.regs[b];
+    }
+
+    #[inline(never)]
+    pub fn sin(&mut self, a: usize, r: usize) {
+        self.regs[r] = self.regs[a].sin();
+    }
+
+    #[inline(never)]
+    pub fn exec_dir(&mut self, prog: &[RegOp], i: f32) -> f32 {
+        self.regs[0] = i;
+
+        if self.regs[0] > 0.5 {
+            self.add(-0.5, 0, 0);
+            self.mul(std::f32::consts::TAU, 0, 0);
+            self.sin(0, 0);
+        } else {
+            self.add(0.3, 0, 1);
+            self.mul(1.1, 1, 1);
+            self.mul2(0, 1, 0);
+        }
+
+        self.regs[0]
     }
 
     #[inline]
@@ -185,6 +258,42 @@ impl RegVM {
                 RegOp::Set(arg, r_r) => {
                     self.regs[r_r as usize] = arg;
                 },
+                RegOp::Fo1 => { self.regs[0] += 0.1; },
+                RegOp::Fo2 => { self.regs[0] += 0.2; },
+                RegOp::Fo3 => { self.regs[0] += 0.3; },
+                RegOp::Fo4 => { self.regs[0] += 0.4; },
+                RegOp::Fo5 => { self.regs[0] += 0.5; },
+                RegOp::Fo6 => { self.regs[0] += 0.6; },
+                RegOp::Fo7 => { self.regs[0] += 0.7; },
+                RegOp::Fo8 => { self.regs[0] += 0.8; },
+                RegOp::Fo9 => { self.regs[0] += 0.9; },
+                RegOp::Fo11 => { self.regs[1] += 0.1; },
+                RegOp::Fo12 => { self.regs[1] += 0.2; },
+                RegOp::Fo13 => { self.regs[1] += 0.3; },
+                RegOp::Fo14 => { self.regs[1] += 0.4; },
+                RegOp::Fo15 => { self.regs[1] += 0.5; },
+                RegOp::Fo16 => { self.regs[1] += 0.6; },
+                RegOp::Fo17 => { self.regs[1] += 0.7; },
+                RegOp::Fo18 => { self.regs[1] += 0.8; },
+                RegOp::Fo19 => { self.regs[1] += 0.9; },
+                RegOp::Fo21 => { self.regs[2] += 0.1; },
+                RegOp::Fo22 => { self.regs[2] += 0.2; },
+                RegOp::Fo23 => { self.regs[2] += 0.3; },
+                RegOp::Fo24 => { self.regs[2] += 0.4; },
+                RegOp::Fo25 => { self.regs[2] += 0.5; },
+                RegOp::Fo26 => { self.regs[2] += 0.6; },
+                RegOp::Fo27 => { self.regs[2] += 0.7; },
+                RegOp::Fo28 => { self.regs[2] += 0.8; },
+                RegOp::Fo29 => { self.regs[2] += 0.9; },
+                RegOp::Fo31 => { self.regs[3] += 0.1; },
+                RegOp::Fo32 => { self.regs[3] += 0.2; },
+                RegOp::Fo33 => { self.regs[3] += 0.3; },
+                RegOp::Fo34 => { self.regs[3] += 0.4; },
+                RegOp::Fo35 => { self.regs[3] += 0.5; },
+                RegOp::Fo36 => { self.regs[3] += 0.6; },
+                RegOp::Fo37 => { self.regs[3] += 0.7; },
+                RegOp::Fo38 => { self.regs[3] += 0.8; },
+                RegOp::Fo39 => { self.regs[3] += 0.9; },
             }
 
             ip += 1;
@@ -211,6 +320,10 @@ const REG_PROG : [RegOp; 8] = [
 
 pub fn variant_reg_vm(vm: &mut RegVM, i: f32) -> f32 {
     vm.exec(&REG_PROG[..], i)
+}
+
+pub fn variant_reg_vm_dir(vm: &mut RegVM, i: f32) -> f32 {
+    vm.exec_dir(&REG_PROG[..], i)
 }
 
 pub fn run() {
@@ -250,6 +363,15 @@ pub fn run() {
     println!("reg Elapsed: {:?} ({})",
              std::time::Instant::now().duration_since(ta), res);
 
+    let ta = std::time::Instant::now();
+    let mut res = 0.0;
+    for i in 0..max {
+        let x = i as f32 / (max as f32);
+        res += variant_reg_vm_dir(&mut rvm, x);
+    }
+
+    println!("rdi Elapsed: {:?} ({})",
+             std::time::Instant::now().duration_since(ta), res);
 }
 
 pub fn main() {
@@ -261,6 +383,8 @@ pub fn main() {
     println!("VM    (0.8) => {}", variant_stack_vm(&mut vm, 0.8));
     println!("REG VM(0.2) => {}", variant_reg_vm(&mut rvm, 0.2));
     println!("REG VM(0.8) => {}", variant_reg_vm(&mut rvm, 0.8));
+    println!("RDI VM(0.2) => {}", variant_reg_vm_dir(&mut rvm, 0.2));
+    println!("RDI VM(0.8) => {}", variant_reg_vm_dir(&mut rvm, 0.8));
     run();
     run();
     run();
